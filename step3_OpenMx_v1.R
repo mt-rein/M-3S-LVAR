63fecdce775e868c4629baf59ebed4d6b43dd63b
mt-rein <m.t.rein@tilburguniversity.edu>
2024-05-08 10:49:51 +0200

    update step3: reorder EM-algorithm

--- step3_OpenMx_v1.R
+++ step3_OpenMx_v1.R
@@ -14,12 +14,12 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
   
   
   #### FOR TESTING ####
-  step2output = output_step2$result$result
-  n_clusters = n_k
-  nstarts = 1
-  maxit = 100
-  structuralmodel = NULL
-  verbose = TRUE
+  # step2output = output_step2$result$result
+  # n_clusters = n_k
+  # nstarts = 3
+  # maxit = 100
+  # structuralmodel = NULL
+  # verbose = TRUE
   
   #### 1) Preparations ####
   ## extract objects from step 1 output:
@@ -47,111 +47,30 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
   udim <- 1 # exogenous covariates (ignored so far)
   ydim <- length(factors) # number of indicators (i.e., factor score variables)
   
-  sv_k1 <- runif(4, -.3, .3)
-  sv_k2 <- runif(4, -.3, .3)
-  if(n_clusters == 4){
-    sv_k3 <- runif(4, -.3, .3)
-    sv_k4 <- runif(4, -.3, .3)
-  }
-  
   ## A matrices (= dynamics)
-  amat_k1 <- mxMatrix(type = "Full", nrow = xdim, ncol = xdim,
-                      free = c(TRUE, TRUE, FALSE, FALSE,
-                               TRUE, TRUE, FALSE, FALSE,
-                               FALSE, FALSE, FALSE, FALSE,
-                               FALSE, FALSE, FALSE, FALSE),
-                      values = c(sv_k1[1], sv_k1[2], 1, 0,
-                                 sv_k1[3], sv_k1[4], 0, 1,
-                                 0, 0, 1, 0,
-                                 0, 0, 0, 1),
-                      name = "A1",
-                      labels = c("phi11_k1", "phi12_k1", NA, NA,
-                                 "phi21_k1", "phi22_k1", NA, NA,
-                                 NA, NA, NA, NA,
-                                 NA, NA, NA, NA),
-                      lbound= c(-.9, -.9, NA, NA,
-                                -.9, -.9, NA, NA,
-                                NA, NA, NA, NA,
-                                NA, NA, NA, NA),
-                      ubound= c(.9, .9, NA, NA,
-                                .9, .9, NA, NA,
-                                NA, NA, NA, NA,
-                                NA, NA, NA, NA),
-                      byrow = TRUE)
-  
-  amat_k2 <- mxMatrix(type = "Full", nrow = xdim, ncol = xdim,
-                      free = c(TRUE, TRUE, FALSE, FALSE,
-                               TRUE, TRUE, FALSE, FALSE,
-                               FALSE, FALSE, FALSE, FALSE,
-                               FALSE, FALSE, FALSE, FALSE),
-                      values = c(sv_k2[1], sv_k2[2], 1, 0,
-                                 sv_k2[3], sv_k2[4], 0, 1,
-                                 0, 0, 1, 0,
-                                 0, 0, 0, 1),
-                      name = "A2",
-                      labels = c("phi11_k2", "phi12_k2", NA, NA,
-                                 "phi21_k2", "phi22_k2", NA, NA,
-                                 NA, NA, NA, NA,
-                                 NA, NA, NA, NA),
-                      lbound= c(-.9, -.9, NA, NA,
-                                -.9, -.9, NA, NA,
-                                NA, NA, NA, NA,
-                                NA, NA, NA, NA),
-                      ubound= c(.9, .9, NA, NA,
-                                .9, .9, NA, NA,
-                                NA, NA, NA, NA,
-                                NA, NA, NA, NA),
-                      byrow = TRUE)
-  
-  if(n_clusters == 4){
-    amat_k3 <- mxMatrix(type = "Full", nrow = xdim, ncol = xdim,
-                        free = c(TRUE, TRUE, FALSE, FALSE,
-                                 TRUE, TRUE, FALSE, FALSE,
-                                 FALSE, FALSE, FALSE, FALSE,
-                                 FALSE, FALSE, FALSE, FALSE),
-                        values = c(sv_k3[1], sv_k3[2], 1, 0,
-                                   sv_k3[3], sv_k3[4], 0, 1,
-                                   0, 0, 1, 0,
-                                   0, 0, 0, 1),
-                        name = "A3",
-                        labels = c("phi11_k3", "phi12_k3", NA, NA,
-                                   "phi21_k3", "phi22_k3", NA, NA,
-                                   NA, NA, NA, NA,
-                                   NA, NA, NA, NA),
-                        lbound= c(-.9, -.9, NA, NA,
-                                  -.9, -.9, NA, NA,
-                                  NA, NA, NA, NA,
-                                  NA, NA, NA, NA),
-                        ubound= c(.9, .9, NA, NA,
-                                  .9, .9, NA, NA,
-                                  NA, NA, NA, NA,
-                                  NA, NA, NA, NA),
-                        byrow = TRUE)
-    
-    amat_k4 <- mxMatrix(type = "Full", nrow = xdim, ncol = xdim,
-                        free = c(TRUE, TRUE, FALSE, FALSE,
-                                 TRUE, TRUE, FALSE, FALSE,
-                                 FALSE, FALSE, FALSE, FALSE,
-                                 FALSE, FALSE, FALSE, FALSE),
-                        values = c(sv_k4[1], sv_k4[2], 1, 0,
-                                   sv_k4[3], sv_k4[4], 0, 1,
-                                   0, 0, 1, 0,
-                                   0, 0, 0, 1),
-                        name = "A4",
-                        labels = c("phi11_k4", "phi12_k4", NA, NA,
-                                   "phi21_k4", "phi22_k4", NA, NA,
-                                   NA, NA, NA, NA,
-                                   NA, NA, NA, NA),
-                        lbound= c(-.9, -.9, NA, NA,
-                                  -.9, -.9, NA, NA,
-                                  NA, NA, NA, NA,
-                                  NA, NA, NA, NA),
-                        ubound= c(.9, .9, NA, NA,
-                                  .9, .9, NA, NA,
-                                  NA, NA, NA, NA,
-                                  NA, NA, NA, NA),
-                        byrow = TRUE)
-  }
+  amat <- mxMatrix(type = "Full", nrow = xdim, ncol = xdim,
+                   free = c(TRUE, TRUE, FALSE, FALSE,
+                            TRUE, TRUE, FALSE, FALSE,
+                            FALSE, FALSE, FALSE, FALSE,
+                            FALSE, FALSE, FALSE, FALSE),
+                   values = c(.1, .1, 1, 0,
+                              .1, .1, 0, 1,
+                              0, 0, 1, 0,
+                              0, 0, 0, 1),
+                   name = "A",
+                   labels = c("phi11", "phi12", NA, NA,
+                              "phi21", "phi22", NA, NA,
+                              NA, NA, NA, NA,
+                              NA, NA, NA, NA),
+                   lbound= c(-.9, -.9, NA, NA,
+                             -.9, -.9, NA, NA,
+                             NA, NA, NA, NA,
+                             NA, NA, NA, NA),
+                   ubound= c(.9, .9, NA, NA,
+                             .9, .9, NA, NA,
+                             NA, NA, NA, NA,
+                             NA, NA, NA, NA),
+                   byrow = TRUE)
   
   # B matrix (= exogenous covariates on latent constructs)
   bmat <- mxMatrix('Zero', nrow = xdim, ncol = udim,
@@ -220,38 +139,42 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
   modelnames <- paste0("id_", unique_ids)
   objectives <- paste(modelnames, "objective", sep = ".")
   model_list_k1 <- list()
+  amat$values[1:2, 1:2] <- runif(4, -.3, .3)                                    # randomize the starting values
   for(i in unique_ids){
     model_list_k1[[i]] <- mxModel(name = modelnames[i],
-                                  amat_k1, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
-                                  mxExpectationStateSpace('A1', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
+                                  amat, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
+                                  mxExpectationStateSpace('A', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
                                   mxFitFunctionML(),
                                   mxData(data[data[, id] == i, factors_ind], 'raw'))
   }
   
   model_list_k2 <- list()
+  amat$values[1:2, 1:2] <- runif(4, -.3, .3)
   for(i in unique_ids){
     model_list_k2[[i]] <- mxModel(name = modelnames[i],
-                                  amat_k2, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
-                                  mxExpectationStateSpace('A2', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
+                                  amat, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
+                                  mxExpectationStateSpace('A', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
                                   mxFitFunctionML(),
                                   mxData(data[data[, id] == i, factors_ind], 'raw'))
   }
   
   if(n_clusters == 4){
     model_list_k3 <- list()
+    amat$values[1:2, 1:2] <- runif(4, -.3, .3)
     for(i in unique_ids){
       model_list_k3[[i]] <- mxModel(name = modelnames[i],
-                                    amat_k3, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
-                                    mxExpectationStateSpace('A3', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
+                                    amat, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
+                                    mxExpectationStateSpace('A', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
                                     mxFitFunctionML(),
                                     mxData(data[data[, id] == i, factors_ind], 'raw'))
     }
     
     model_list_k4 <- list()
+    amat$values[1:2, 1:2] <- runif(4, -.3, .3)
     for(i in unique_ids){
       model_list_k4[[i]] <- mxModel(name = modelnames[i],
-                                    amat_k4, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
-                                    mxExpectationStateSpace('A4', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
+                                    amat, bmat, cmat, dmat, qmat, rmat, xmat, pmat, umat,
+                                    mxExpectationStateSpace('A', 'B', 'C', 'D', 'Q', 'R', 'x0', 'P0', 'u'),
                                     mxFitFunctionML(),
                                     mxData(data[data[, id] == i, factors_ind], 'raw'))
     }
@@ -284,8 +207,16 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
     
     # loop over iterations until convergence or max iterations are reached
     for(it in 1:maxit) {
-      ## M-step: fitting SSM model and update class proportions
+      ## E-step: update class membership (skip in first iteration) and class proportions
+      if(it >1){
+        # update posteriors:
+        post <- EStep(pi = class_proportions, ngroup = n, nclus = n_clusters, loglik = casewise.loglik)
+      }
       
+      # compute class proportions:
+      class_proportions <- colMeans(post)
+      
+      ## M-step: fitting SSM model and update parameter estimates
       # create a multi-person model for each latent class
       # the fit function is weighted by the posterior probabilities of each person-model
       # create the mxModel for cluster 1:
@@ -315,11 +246,7 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
         model_k4r <- mxRun(model_k4, silent = !verbose, suppressWarnings = TRUE)
       }
       
-      # compute class proportions:
-      class_proportions <- colMeans(post)
-      
-      ## E-step
-      # computing casewise log likelihoods in all models
+      ## update LL:
       fitfunctions <- paste0(modelnames, ".fitfunction")
       m2_loglik_k1 <- purrr::map_dbl(fitfunctions, mxEvalByName, model=model_k1r)
       m2_loglik_k2 <- purrr::map_dbl(fitfunctions, mxEvalByName, model=model_k2r)
@@ -337,13 +264,12 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
       # transform on log likelihood scale:
       casewise.loglik <- casewise.m2_loglik/(-2)
       
+      
       # compute complete-data log likelihood
       complete_data_loglik <-  sum(post*log(class_proportions)-post*casewise.loglik)
       
       # compute observed-data log likelihood
       observed_data_loglik <- sum(log(rowSums(class_proportions*exp(casewise.loglik))))
-      
-      
       
       if(verbose){
         if(it != 1){
@@ -353,22 +279,24 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
         }
         
       }
+      
+      
       # check convergence:
       if(it>1 && (observed_data_loglik - observed_data_loglik0)<1.0e-6){
         if(verbose){
           print("Convergence achieved.")
         }
-        if(total_loglik < loglik0){
-          fullmodelr <- fullmodelr0
-          total_loglik <- loglik0
+        if(n_clusters == 2){
+          models <- list(model_k1r, model_k2r)
         }
+        if(n_clusters == 4){
+          models <- list(model_k1r, model_k2r, model_k3r, model_k4r)
+        }
+        
         break
       }
-      # update posteriors:
-      post <- EStep(pi = class_proportions, ngroup = n, nclus = n_clusters, loglik = casewise.loglik)
       
-      loglik0 = total_loglik
-      fullmodelr0 = fullmodelr
+      observed_data_loglik0 = observed_data_loglik
       
       if(it == maxit){
         if(verbose){
@@ -381,16 +309,16 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
     # check if the new fit is better than the best one from previous random starts
     if(i == 1){
       # if it's the first random start, save results
-      best_loglik <- total_loglik
+      best_loglik <- observed_data_loglik
       best_post <- post
+      best_models <- models
       most_iterations <- it
-      best_model <- fullmodelr
     } else {
       # otherwise, compare fits
-      if(total_loglik > best_loglik){
-        best_loglik <- total_loglik
+      if(observed_data_loglik > best_loglik){
+        best_loglik <- observed_data_loglik
         best_post <- post
-        best_model <- fullmodelr
+        best_models <- models
       }
       if(it > most_iterations){
         most_iterations <- it
@@ -408,8 +336,8 @@ step3 <- function(step2output, structuralmodel = NULL, n_clusters,
   
   #### 5) extract estimates ####
   ## TO DO: make this more flexible 
-  # cluster 1:
-  estimates <- coef(best_model)
+  estimates <- lapply(best_models, coef)
+  names(estimates) <- paste0("cluster", 1:n_clusters)
   
   loglik <- best_loglik
   post <- best_post
